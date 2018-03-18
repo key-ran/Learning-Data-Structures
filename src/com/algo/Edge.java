46c2423af6417da6539c74a7687f4594d274624f
Kiran <kirankumargkwd@gmail.com>
2018-03-18 21:13:49 +0530

    Edge and Node Doc Update

--- src/com/algo/Edge.java
+++ src/com/algo/Edge.java
@@ -5,26 +5,59 @@ public class Edge {
 	private int toNodeIndex;
 	private int length;
 
+	/**
+	 * Class constructor specifying an edge to be created with FROM and TO
+	 * indices with a weight.
+	 * 
+	 * @param fromNodeIndex
+	 *            Index of FROM Node
+	 * @param toNodeIndex
+	 *            Index of TO Node
+	 * @param length
+	 *            Weight of the Edge
+	 */
 	public Edge(int fromNodeIndex, int toNodeIndex, int length) {
 		this.fromNodeIndex = fromNodeIndex;
 		this.toNodeIndex = toNodeIndex;
 		this.length = length;
 	}
 
+	/**
+	 * Get the Index of the FROM Node
+	 * 
+	 * @return Index value of FROM Node
+	 */
 	public int getFromNodeIndex() {
 		return fromNodeIndex;
 	}
 
+	/**
+	 * Get the Index of the TO Node
+	 * 
+	 * @return Index value of TO Node
+	 */
 	public int getToNodeIndex() {
 		return toNodeIndex;
 	}
 
+	/**
+	 * Get the Length of current Edge
+	 * 
+	 * @return weight of current Edge
+	 */
 	public int getLength() {
 		return length;
 	}
 
-	// determines the neighbouring node of a supplied node, based on the two
-	// nodes connected by this edge
+	/**
+	 * Determines the nearby Node of the current Node, based on the edge
+	 * connecting the two nodes
+	 * 
+	 * @param nodeIndex
+	 *            Index of current Node
+	 * 
+	 * @return Index of nearby Node
+	 */
 	public int getNeighbourIndex(int nodeIndex) {
 		if (this.fromNodeIndex == nodeIndex) {
 			return this.toNodeIndex;
--- src/com/algo/Node.java
+++ src/com/algo/Node.java
@@ -3,32 +3,74 @@ package com.algo;
 import java.util.ArrayList;
 
 public class Node {
+
 	private int distanceFromSource = Integer.MAX_VALUE;
 	private boolean visited;
-	private ArrayList<Edge> edges = new ArrayList<Edge>(); // now we must create
-															// edges
+	private ArrayList<Edge> edges = new ArrayList<Edge>();
 
+	/**
+	 * Get Distance from Source Node to current Node
+	 * 
+	 * @return Integer value of distance from Source Node
+	 */
 	public int getDistanceFromSource() {
 		return distanceFromSource;
 	}
 
+	/**
+	 * Set Distance from Source Node to current Node
+	 *
+	 * @param distanceFromSource
+	 *            Integer value of distance from Source Node
+	 */
 	public void setDistanceFromSource(int distanceFromSource) {
 		this.distanceFromSource = distanceFromSource;
 	}
 
+	/**
+	 * Checks if the Node is visited previously
+	 * 
+	 * @return
+	 *         <p>
+	 *         Flag = true (visited)
+	 *         </p>
+	 *         <p>
+	 *         Flag = false (not visited)
+	 *         </p>
+	 * 
+	 */
 	public boolean isVisited() {
 		return visited;
 	}
 
+	/**
+	 * Set visited flag to current Node
+	 * 
+	 * @param visited
+	 *            Flag to be set to the current Node
+	 * 
+	 */
 	public void setVisited(boolean visited) {
 		this.visited = visited;
 	}
 
+	/**
+	 * Get all the edges of the current Node
+	 * 
+	 * @return List of Edges to the current Node
+	 */
 	public ArrayList<Edge> getEdges() {
 		return edges;
 	}
 
+	/**
+	 * Set all the edges to the current Node
+	 * 
+	 * @param edges
+	 *            List of all edges to current Node
+	 */
 	public void setEdges(ArrayList<Edge> edges) {
 		this.edges = edges;
 	}
+
 }
